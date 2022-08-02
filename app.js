535b5c636a64640ad51b818d78f21182026a68d3
Manish Sharma <manish1206s@gmail.com>
2022-08-02 12:16:26 +0530

    UI Update

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-app.use('/static', express.static('./frontend/resources/images'));
+//static files
+app.use('/static', express.static('./public/views/images'));
+app.use('/css' , express.static('./public/css'))
+//set templating engine
+app.set('view engine' , 'ejs');
+app.set('views' , 'public/views');
+app.use(expressLayouts);
 
-app.use('/',(req,res)=>{
-    return res.sendFile('frontend/index.html',{root:__dirname});
+//navigation
+
+app.use('/contact',(req,res)=>{
+    return res.render('contact.ejs' , {title : 'contact'})
 })
+app.use('/signup',(req,res)=>{
+    return res.render('signup.ejs' , {title : 'signup'})
+})
+app.use('/login',(req,res)=>{
+    return res.render('login.ejs' , {title : 'login'})
+})
+app.use('/about',(req,res)=>{
+    return res.render('about.ejs' , {title : 'about'})
+})
+app.use('/',(req,res)=>{
+    return res.render('index.ejs' , {title : 'home'})
+})
+
+
